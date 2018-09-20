2f4bc5929170a6ccd3e55492f90646665a978830
Folsom <Scott.Folsom@CenturyLink.com>
2018-09-20 09:36:23 -0500

    Separated starship model to own file

--- index.md
+++ index.md
@@ -1,3 +1,5 @@
 ## StarTrekPy
 
 ## A love-letter to 1970s mainframe gaming in broken Python
+
+Currently not functional.
--- main.py
+++ main.py
@@ -4,57 +4,13 @@
 
 #imports as needed
 from math import sqrt
+from models import starship
 import random
 
 #right now the map is a global, might change later
 map = []
 
-class starship():
-    """
-    This class is used for all movable starship objects, both player and NPC.
-    """
 
-    def __init__(self, name, shields, hull, phaser, photons, coordX, coordY):
-        """
-        The class constructor!
-        """
-        self.name = name
-        self.shields = shields
-        self.hull = hull
-        self.phaser = phaser
-        self.photons = photons
-        self.coordX = coordX
-        self.coordY = coordY
-        self.location = (coordX, coordY)
-
-    def move(self, newX, newY):
-        """
-        Method that allows a ship to move along the map
-        """
-        map[enterprise.coordX-1][enterprise.coordY-1] = "." #prevents the Picard Maneuver
-        map[klingon.coordX-1][klingon.coordY-1] = "."
-        self.coordX = newX
-        self.coordY = newY
-        self.location = (newX,newY)
-
-    def phaser_attack(self, target, strength):
-        """
-        Attack an opposing starship using, depleting its shields or hull.
-        In the current implementation, the phaser strike weakens as a direct
-        function of the distance of the shot.
-        """
-        phaser_intensity = strength - sqrt((self.coordX - target.coordX)**2 + (self.coordY - target.coordY)**2)
-        self.phaser = self.phaser - phaser_intensity
-        target.shields = target.shields - phaser_intensity
-        print(self.name + " attacks "  + target.name + " for " +  str(phaser_intensity) + " damage.")
-
-    def photon_attack(self, target, torp_count):
-        """
-        Photon torpedo attack; damage varies based on number of torpedos used.
-        """
-        photon_intensity = 8 + 2 * torp_count
-        self.photons = self.photons - torp_count
-        target.hull = target.hull - photon_intensity
 
 #let's make an example starship
 enterprise = starship("enterprise", 100, 100, 100, 100, 3, 3)
